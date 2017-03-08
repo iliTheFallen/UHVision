e7bdbdd13414caf1e3c05221ed643be94150bf53
Ilker GURCAN <gurcan.ilker@gmail.com>
2017-03-08 16:59:27 -0600

    Committing changes for GPU machine.

--- SelfDrivingCar/model/alex_net.py
+++ SelfDrivingCar/model/alex_net.py
@@ -28,39 +28,73 @@
 '''
 
 from tflearn import DNN
+from tflearn.initializations import normal as normal
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.normalization import local_response_normalization
+from tflearn.optimizers import Momentum
 from tflearn.layers.estimator import regression
 
+import tensorflow as tf
 
-def build_alex_net(input_shape, num_classes):
+NUM_EPOCHS=10
 
+
+def build_alex_net(input_shape,
+                   num_classes):
+
+    print('Building AlexNet...')
     # input_shape does not have the dimension for batch size.
     # It is specified at training step. That is why we have
     # None for the 1st dimension
-    print('Building AlexNet...')
-    network = input_data(shape=[None, input_shape])
-    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    input_shape.insert(0, None)
+    # Building network...
+    network = input_data(shape=input_shape)
+    weight_init = normal(shape=[input_shape[1]*input_shape[2], ], mean=0.0, stddev=0.01)
+    network = conv_2d(network,
+                      96, 11, strides=4,
+                      activation='relu',
+                      weights_init=weight_init)
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = conv_2d(network, 256, 5, activation='relu')
+    network = conv_2d(network,
+                      256, 5, activation='relu',
+                      weights_init=weight_init,
+                      bias_init=tf.constant_initializer(1.0))
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 256, 3, activation='relu')
+    network = conv_2d(network,
+                      384, 3, activation='relu',
+                      weights_init=weight_init)
+    network = conv_2d(network,
+                      384, 3, activation='relu',
+                      weights_init=weight_init,
+                      bias_init=tf.constant_initializer(1.0))
+    network = conv_2d(network,
+                      256, 3, activation='relu',
+                      weights_init=weight_init,
+                      bias_init=tf.constant_initializer(1.0))
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
-    network = fully_connected(network, 4096, activation='tanh')
+    network = fully_connected(network,
+                              4096, activation='tanh',
+                              weights_init=weight_init)
     network = dropout(network, 0.5)
-    network = fully_connected(network, 4096, activation='tanh')
+    network = fully_connected(network,
+                              4096, activation='tanh',
+                              weights_init=weight_init)
     network = dropout(network, 0.5)
-    network = fully_connected(network, num_classes, activation='softmax')
+    network = fully_connected(network,
+                              num_classes, activation='softmax',
+                              weights_init=weight_init)
+    # decayed_learning_rate = learning_rate *  decay_rate ^ (global_step / decay_steps)
+    momentum = Momentum(learning_rate=0.01,
+                        lr_decay=0.0005,
+                        decay_step=input_shape[0]*NUM_EPOCHS,
+                        momentum=0.9)
     network = regression(network,
-                         optimizer='momentum',
-                         loss='categorical_crossentropy',
-                         learning_rate=0.001)
+                         optimizer=momentum,
+                         loss='categorical_crossentropy')
     return network
 
 
@@ -77,12 +111,12 @@ def train(network,
                 tensorboard_verbose=2)
     model.fit(input_,
               labels,
-              n_epoch=10,
-              validation_set=0.1,
+              n_epoch=NUM_EPOCHS,
+              validation_set=0.1,  # 10% of training data will be used for validation
               shuffle=True,
               show_metric=True,
               batch_size=batch_size,
-              snapshot_step=200,
-              snapshot_epoch=False,
+              snapshot_step=batch_size,  # Save the model at every this many steps.
+              snapshot_epoch=False,  # We don't want snapshot at the end of each epoch
               run_id=model_name)
     return model
--- SelfDrivingCar/omniglot_test.py
+++ SelfDrivingCar/omniglot_test.py
@@ -28,17 +28,45 @@
 '''
 
 import time as time
+import numpy as np
 
 from metalearning_tf.data.generators import OmniglotGenerator
 
 import model.alex_net as alex
+
+IM_W = 20
+IM_H = 20
+
+
+def preprocess_data(omniglot_generator):
+
+    X = None
+    y = None
+    print('Preprocessing input data...')
+    for e, (input_, target) in omniglot_generator:
+        print('Current episode: %d' % e)
+        # There is no sequence notion in this context. Our input_ will be of size:
+        # (BS.SL)x(WxH). A 3D cube whose each slight is an input_
+        in_shape = input_.shape
+        input_ = input_.reshape(in_shape[0]*in_shape[1], in_shape[2])
+        input_ = input_.reshape(in_shape[0]*in_shape[1], IM_W, IM_H)
+        tar_shape = target.shape
+        target = target.reshape(tar_shape[0]*tar_shape[1])
+        # Stack them up vertically
+        if X is None:
+            X = input_
+            y = target
+        else:
+            X = np.concatenate((X, input_), axis=0)
+            y = np.concatenate((y, target), axis=0)
+    return X, y
 
 
 def main():
 
     # Load data as was defined in the original code in Theano
     # As in Alex's paper; data augmentation is provided via shifting/rotation
-    sample_generator = OmniglotGenerator(data_folder='./omni_samples/',
+    sample_generator = OmniglotGenerator(data_folder='../MetaLearningTF/omni_samples/',
                                          batch_size=16,  # Each call to the next() operator fetches this many samples
                                          nb_samples=5,  # Actually, it is # of classes
                                          nb_samples_per_class=10,  # # of samples per class
@@ -48,13 +76,14 @@ def main():
     st = time.time()
     try:
         # Build the network
-        alex.build_alex_net([20, 20], sample_generator.nb_samples)
+        network = alex.build_alex_net([IM_W, IM_H], sample_generator.nb_samples)
         # Extract training data
-
+        input_, labels = preprocess_data(sample_generator)
         # Train the network
-
+        model = alex.train(network, input_, labels, 16, 'alexnet_model')
     except KeyboardInterrupt:
         print('Elapsed Time: %ld' %(time.time()-st))
+        pass
 
 
 if __name__ == "__main__":
